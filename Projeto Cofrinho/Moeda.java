06ad0a603b71ca47baf94d4da2dbf38b0225840c
bellegallo <148583462+bellegallo@users.noreply.github.com>
2023-10-20 19:53:49 -0300

    Create Moeda.java

---
+++ Projeto Cofrinho/Moeda.java
@@ -0,0 +1,11 @@
+package empresa;
+
+public abstract class Moeda { //Classe abstrata para criar outras classes parecidas
+	
+	protected double valor;
+	
+	public abstract void info();
+	public abstract double converter();
+	
+
+}
