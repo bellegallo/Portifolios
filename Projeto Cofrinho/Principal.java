82ac6a86226fdd5893ac2aa82d0341946ba26869
bellegallo <148583462+bellegallo@users.noreply.github.com>
2023-10-20 19:54:10 -0300

    Create Principal.java

---
+++ Projeto Cofrinho/Principal.java
@@ -0,0 +1,15 @@
+package empresa;
+
+import java.util.ArrayList;
+
+
+public class Principal {
+
+	//Classe principal, com funções que exibirão o menu de escolha
+	public static void main(String[] args) {
+		Menu menu = new Menu();
+		menu.exibirMenu();
+		
+		
+
+}}
